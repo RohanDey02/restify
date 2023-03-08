4fd1700f206c82caabba9d82b145d96bcd218c1b
saeedahsan <ahsan02@gmail.com>
2023-03-07 20:12:26 -0500

    Added comments urls.py

--- P2/restify/restify/urls.py
+++ P2/restify/restify/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('accounts/', include('accounts.urls')),
-    path('property/', include('properties.urls'))
+    path('property/', include('properties.urls')),
+    path('comments/', include('comments.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
