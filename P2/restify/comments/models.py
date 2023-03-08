601a4e00c3e019dc334e299b2bbbf2dcac24e642
saeedahsan <ahsan02@gmail.com>
2023-03-08 10:52:28 -0500

    Fixed comments models

--- P2/restify/comments/models.py
+++ P2/restify/comments/models.py
@@ -1,16 +1,18 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.db.models.deletion import SET_NULL
-
-class Comment(models.Model):
-    message = models.TextField(blank=False, null=False)
-    sender_type = models.CharField(max_length=255, blank=False, null=False)
-    last_modified = models.DateTimeField(auto_now=True)
-
-    def __str__(self) -> str:
-        return f"{self.sender_type} // {self.last_modified}"
+from P2.restify.reservations.models import Reservation
 
 class Feedback(models.Model):
     user_rating = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(5.0)], blank=True, null=True)
     property_rating = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(5.0)], blank=True, null=True)
-    comments = models.ForeignKey(Comment, on_delete=models.SET_NULL, null=True)
+    reservation = models.OneToOneField(Reservation, on_delete=models.CASCADE, null=True)
+    
+class Comment(models.Model):
+    message = models.TextField(blank=False, null=False)
+    sender_type = models.CharField(max_length=255, blank=False, null=False)
+    last_modified = models.DateTimeField(auto_now=True)
+    feedback = models.ForeignKey(Feedback, on_delete=models.CASCADE, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.sender_type} // {self.last_modified}"
