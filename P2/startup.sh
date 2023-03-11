806d88926c51d4b60c0880fe1bbf918e7bb64a1c
RohanDey02 <58871401+RohanDey02@users.noreply.github.com>
2023-03-11 11:56:22 -0500

    Made changes to scripts to work on Ubuntu

--- P2/startup.sh
+++ P2/startup.sh
@@ -1,8 +1,26 @@
+#!/bin/bash
+
+# Check if Python is installed
+python3.11 --version
+if [ `echo $?` -eq 127 ] ; then
+    sudo add-apt-repository ppa:deadsnakes/ppa
+    sudo apt update
+    sudo apt install python3.11
+    sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 1
+fi
+
+pip3 --version
+if [ `echo $?` -eq 127 ] ; then
+    sudo apt install python3-pip
+fi
+
+curl -sS https://bootstrap.pypa.io/get-pip.py | sudo python3.11
+
 # Go back to current directory
 cd restify
 
 # Install virtualenv
-pip install virtualenv
+pip3 install virtualenv
 
 # Create folder and activate
 virtualenv venv 
@@ -10,7 +28,9 @@ chmod +x venv/bin/activate
 source venv/bin/activate
 
 # Install dependencies
-pip install -r packages.txt
+pip3 install -r packages.txt
 
-./manage.py makemigrations
-./manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+cd ..
