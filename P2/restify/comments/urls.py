3f097fb459944340dc1f7afe7801448b49ebc461
saeedahsan <ahsan02@gmail.com>
2023-03-12 17:15:24 -0400

    Bug fixes and added endpoint to comments views

--- P2/restify/comments/urls.py
+++ P2/restify/comments/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name="comments"
 urlpatterns = [
     path('create/', views.CreateComment.as_view(), name="create"),
+    path('<int:id>/', views.get_comment, name="get_comment"),
     path('<int:id>/allPropertyFeedbackComments/', views.get_all_property_feedback_comments, name="get_all_property_feedback_comments"),
     path('<int:id>/allGuestFeedbackComments/', views.get_all_guest_feedback_comments, name="get_all_guest_feedback_comments"),
     path('<int:id>/propertyConversationComments/', views.get_property_conversation_comments, name="get_property_conversation_comments"),
