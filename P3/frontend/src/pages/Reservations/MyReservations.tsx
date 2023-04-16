f1374b3945fd9fb3710a7daa1bb9033abc7f77e2
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2023-04-16 02:21:40 -0400

    added comments

--- P3/frontend/src/pages/Reservations/MyReservations.tsx
+++ P3/frontend/src/pages/Reservations/MyReservations.tsx
@@ -9,13 +9,19 @@ import { Pagination } from "./Pagination";
 import DisplayReservations from "./DisplayReservations";
 
 export default function MyReservations() {
+	// grab user info and access token
 	const { tokens, data } = useToken();
+	// state for the selected reservation, used for the Status modal
 	const [selectedRes, setSelectedRes] = useState<Reservation | null>(null);
 	const access_token = tokens.access;
+
+	// states for pagination and filtering
 	const [pageNum, setPageNum] = useState(1);
 	const [entriesPerPage, setEntriesPerPage] = useState(3);
 	const [nextExists, setNextExists] = useState(true);
 	const [openFiltering, setOpenFiltering] = useState(false);
+
+	// state for filtering information, used to filter the reservations
 	const [filteringInfo, setFilteringInfo] = useState<{
 		sortBy: string;
 		stateToFilterBy: string;
@@ -32,9 +38,11 @@ export default function MyReservations() {
 	return (
 		<div className="w-full min-h-screen p-2 bg-gray-100">
 			<Navbar currentLocation="/myreservations" firstName={data.first_name} />
+			{/* title of the page */}
 			<h2 className="text-center text-2xl font-bold text-teal-700 sm:text-3xl pt-3">
 				Reservations
 			</h2>
+			{/* contains button to open filtering modal and the modal itself which handles filtering */}
 			<FilteringModal
 				entriesPerPage={entriesPerPage}
 				filteringInfo={filteringInfo}
@@ -43,16 +51,19 @@ export default function MyReservations() {
 				setFilteringInfo={setFilteringInfo}
 				setOpenFiltering={setOpenFiltering}
 			/>
+			{/* takes a list of reservations and displays cards, also handles sorting the reservations based on the filtering information state */}
 			<DisplayReservations
 				reservations={reservations}
 				setSelectedRes={setSelectedRes}
 				filteringInfo={filteringInfo}
 			/>
+			{/* Contains SVGs used for arrows in sorting and displays current page num */}
 			<Pagination
 				nextExists={nextExists}
 				pageNum={pageNum}
 				setPageNum={setPageNum}
 			/>
+			{/* handles all the logic related to the popups for the cards themself, here you can change status of reservations, view original listing etc */}
 			<StatusModal
 				selectedRes={selectedRes}
 				setSelectedRes={setSelectedRes}
